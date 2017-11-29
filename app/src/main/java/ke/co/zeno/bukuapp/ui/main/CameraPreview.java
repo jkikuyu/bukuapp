e642fdd76399f0faab07d1679b859a9988f14256
Jude Kikuyu <jkikuyu@gmail.com>
2017-11-29 23:55:07 +0300

    add photohandler and camera preview

---
+++ app/src/main/java/ke/co/zeno/bukuapp/ui/main/CameraPreview.java
@@ -0,0 +1,12 @@
+package ke.co.zeno.bukuapp.ui.main;
+
+import android.view.SurfaceView;
+
+import Surface.Holder;
+
+/**
+ * Created by nimda on 16/11/17.
+ */
+
+public class CameraPreview extends SurfaceView implements Holder {
+}
---
+++ app/src/main/java/ke/co/zeno/bukuapp/ui/main/adapter/PhotoHandler.java
@@ -0,0 +1,10 @@
+package ke.co.zeno.bukuapp.ui.main.adapter;
+
+import android.hardware.Camera;
+
+/**
+ * Created by nimda on 18/11/17.
+ */
+
+public class PhotoHandler implements Camera.PictureCallback {
+}
